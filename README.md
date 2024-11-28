5016cd2616a19213eb74c0d7ad88995b4c19706c
fabianwolski <fabianwolski03@gmail.com>
2024-11-28 22:11:20 +0000

    Both Emulator and Production run options for project

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 -run backend
 firebase login
-npm run srv
+npm run prod
 
 -if you want to add new emulator run
 firebase init emulators
+
+-using this for unix scripts on windows
+npm install env-cmd --save-dev
--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,10 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 import { getAuth, connectAuthEmulator } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
@@ -14,16 +12,32 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Export firestore database
-// It will be imported into your react app whenever it is needed
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+//testing logs, no longer using NODE_ENV instead our own one
+console.log('Node Environment:', process.env.NODE_ENV);
+console.log('App Environment:', process.env.REACT_APP_ENV);
+//first 6 chars of api key for dev use only
+// console.log('apikey', firebaseConfig.apiKey?.substring(0,6))
 
-if(process.env.NODE_ENV === "development"){
-  connectFirestoreEmulator(db, "localhost", 8080)
-  connectAuthEmulator(auth,"http://localhost:9099");
+
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+if (process.env.REACT_APP_ENV === 'development') {
+  console.log("Using Firebase Emulators");
+  
+  connectAuthEmulator(auth, "http://localhost:9099", { disableWarnings: true });
+  connectFirestoreEmulator(db, "localhost", 8080);
+  
+  //testing auth connection
+  // auth.useDeviceLanguage();
+  // console.log("Auth emulator URL:", auth.config.emulator?.url);
+  // console.log("Firebase config:", firebaseConfig);
+} else{
+  console.log('Using Production Firebase');
 }
 
+export {auth, db}
 export default app;
