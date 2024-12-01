328cb2e7b3dc6017b596b8e3f91b917bb60d2add
fabianwolski <fabianwolski03@gmail.com>
2024-12-01 13:13:19 +0000

    components/page code restructure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 //import ComponentX from './components/ComponentX'
 import React, {useState, useEffect} from 'react';
-import Home from './page/Home'
-import Login from './page/Login'
-import Signup from './page/Signup'
+import Home from './components/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
 import {BrowserRouter as Router} from 'react-router-dom';
 import {Routes, Route} from 'react-router-dom';
 function App() {
