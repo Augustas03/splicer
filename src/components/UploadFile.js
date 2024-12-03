bcd1fc248e2f566cd63ed9ea9557eed5e48cfce6
fabianwolski <fabianwolski03@gmail.com>
2024-12-03 11:13:21 +0000

    feature: drag/drop functionality. Universal processFile function added

--- src/components/UploadFile.js
+++ src/components/UploadFile.js
@@ -1,4 +1,4 @@
- import { useContext, useRef } from 'react'
+ import { useContext, useRef, useState } from 'react'
  import { useNavigate } from 'react-router-dom'
  import  {FileContext}  from '../contexts/Context'
 
@@ -7,41 +7,81 @@ const UploadFile = () =>{
     const navigate = useNavigate()
     const fileInputRef = useRef(null)
     const { setFileURL } = useContext(FileContext)
+     //state for drag status
+    const [isDragging, setIsDragging] = useState(false)
     
+    //function that handles any file uploaded directly or dropped
+    const processFile = (file) => {
+        // Safety check - if no file, exit the function
+        if(!file){
+            console.log("file empty")
+            return
+        }
+         //try create url and set the file
+        try{
+            const fileURL = URL.createObjectURL(file)
+            setFileURL(fileURL)
+            navigate('/edit')
+        }catch(error){
+            console.log(error)
+        }
+    }
 
     const handleFileUpload = (e) =>{
         //we need to get the file .target
         const selectedFile = e.target.files[0]
-
-        if(!selectedFile){
-            console.log("file empty")
-            return
-        }
-        
-        //try create url and set the file
-        try{
-            const fileURL = URL.createObjectURL(selectedFile)
-            setFileURL(fileURL)
-            navigate('/edit')
-        } catch(err){
-            console.log(err)
-        }
+        processFile(selectedFile)
     }
 
-    const handleDrag = () =>{
-        console.log("drag is called")
+    //drag drop functionality
+    //later remove logs!!!
+    //useful for styling
+    const handleDragIn = (e) =>{
+        //stopPropagation is like saying stops here dont tell my parents
+        //which could be other nested divs etc.
+        e.preventDefault()
+        e.stopPropagation()
+        console.log("dropped inside is called")
+        setIsDragging(true)
+    }
+    const handleDragOut = (e) =>{
+        e.preventDefault()
+        e.stopPropagation()
+
+        setIsDragging(false)
+        console.log("draggedout is called")
     } 
-    const handleDrop = () =>{
-        console.log("drop is called")
+    //Fires nonstop while a file is being dragged over the drop zone
+    const handleDragHover = (e) =>{
+        e.preventDefault()
+        e.stopPropagation()
+        
+        console.log("hovering...")
+    }   
+    const handleDrop = (e) =>{
+        e.preventDefault()
+        e.stopPropagation()
+        setIsDragging(false)
+
+        const droppedFile = e.dataTransfer.files[0]
+        processFile(droppedFile)
+        console.log("file dropped")
     }
     
     return(
         <main>
             <section>
+        {/* Temporary styling on drop zone div, edit later*/}
         <div 
-        onDragOver={handleDrag}
-        onDrop={handleDrop}
-        role="button">
+        onDragEnter={handleDragIn}     
+        onDragLeave={handleDragOut}     
+        onDragOver={handleDragHover}       
+        onDrop={handleDrop}               
+        style={{
+            border: isDragging ? '2px solid blue' : '2px solid black',
+            padding: '20px'
+        }}
+        > Drop Zone
 
             <label htmlFor='upload-file'></label>
             {/* Button is just UI */}
