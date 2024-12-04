2a7a8dc6896f3776b70c1ab0e85216073e60cd1c
fabianwolski <fabianwolski03@gmail.com>
2024-12-04 11:48:52 +0000

    Full Styling For Editing Audio and waveform + Play&Pause/Restart/SkipIn/SkipOut

---
+++ src/components/audio/Audio.js
@@ -0,0 +1,101 @@
+import {useEffect, useRef, useContext} from 'react';
+import {FileContext} from '../../contexts/Context';
+import WaveSurfer from 'wavesurfer.js';
+import './Audio.css';
+
+import{
+    BsSkipBackward,
+    BsSkipForward,
+    BsFillStopFill,
+    BsFillPlayFill
+} from "react-icons/bs"
+ 
+const Audio = () =>{
+    //this is for rendering
+    const waveFormRef = useRef(null)
+    //this is for storing
+    const waveSurferRef = useRef(null)
+
+    const {fileURL} = useContext(FileContext)
+    
+    useEffect(() =>{
+        //we are using useRef to avoid re-rendering each time a change occurs 
+        //in component. Imagine user is playing with the waveform and
+        //component re-renders due to parent update,state change etc
+        //could cause lots of issues with play/pause clicking etc.
+
+        //.current gives direct access to whatever is stored in the ref
+        //so entire wavesurfer instance will all properties and methods
+        waveSurferRef.current = WaveSurfer.create({
+            //use DOM element for rendering container
+            container: waveFormRef.current,
+            waveColor: '#34374B',
+            progressColor: '#F90',
+            dragToSeek: true,
+            width: '100%',
+            height: 92,
+            hideScrollbar: true,
+            normalize: true,
+            barWidth: 3,         
+            barGap: 3,           
+            barHeight: 1,        
+            barRadius: 3,        
+            padding: 0
+        });
+ 
+        if(fileURL){
+            waveSurferRef.current.load(fileURL)
+        }
+ 
+        return () => waveSurferRef.current.destroy()
+    }, [fileURL]);
+
+    const handleStop= () =>{
+        if(waveSurferRef.current){
+            waveSurferRef.current.stop()
+        }
+    }
+
+    const handlePlayPause =() => {
+        if(waveSurferRef.current){
+            waveSurferRef.current.playPause()
+        }
+    }
+
+    const handleSkipForward = () =>{
+        if(waveSurferRef.current){
+            waveSurferRef.current.skip(2)
+        }
+    }
+    const handleSkipBack = () =>{
+        if(waveSurferRef.current){
+            waveSurferRef.current.skip(-2)
+        }
+    }
+
+
+
+    return (
+        <div className="player-container">
+        <div className="player-box">
+            <div ref={waveFormRef} className="wave-container">
+            </div>
+            <div className="controls-container">
+                <button onClick={handleSkipBack} className="control-button">
+                    <BsSkipBackward/>
+                </button>
+                <button onClick={handlePlayPause} className="control-button play-button">
+                    <BsFillPlayFill/>
+                </button>
+                <button onClick={handleStop} className="control-button stop-button">
+                    <BsFillStopFill/>
+                </button>
+                <button onClick={handleSkipForward} className="control-button">
+                    <BsSkipForward/>
+                </button>
+            </div>
+        </div>
+    </div>
+      );
+}
+export default Audio;
---
+++ src/pages/edit/Edit.js
@@ -0,0 +1,10 @@
+import Audio from '../../components/audio/Audio'
+import './Edit.css'
+const Edit = () =>{
+    return(
+        <div className="edit">
+            <Audio/>
+        </div>
+    )
+}
+export default Edit;
