a88e99cd9e19fc464f88b6412914b71b8fd04960
fabianwolski <fabianwolski03@gmail.com>
2024-12-04 12:38:22 +0000

    Added scalability for routes without header necessary and default=with header

--- src/App.js
+++ src/App.js
@@ -4,29 +4,44 @@ import Login from './components/Login'
 import Signup from './components/Signup'
 import Header from './pages/Header'
 import UsersLayout from './layouts/UsersLayout'
-import Edit from './pages/Edit'
+import Edit from './pages/edit/Edit'
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <section>
-            <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/users" element={<UsersLayout/>}>
-              {/* Default redirect to login if in '/users' route by itself */}
-              <Route index element={<Navigate to="/" replace/>}/>
-              {/* Remove the /users/ prefix from child routes */}
-              <Route path="login" element={<Login/>} />
-              <Route path="signup" element={<Signup/>} />
-            </Route>
-            <Route path="edit" element={<Edit/>} />
-          </Routes>
-        </section>
-      </div>
-    </Router>
+  const DefaultLayout = ({ children }) => (
+    <>
+      <Header />
+      {children}
+    </>
   );
+
+    return (
+      <Router>
+        <div>
+          <section>
+            <Routes>
+              {/* Routes with header*/}
+              <Route path="/" element={
+                <DefaultLayout>
+                  <Home />
+                </DefaultLayout>
+              } />
+              <Route path="/users" element={
+                <DefaultLayout>
+                  <UsersLayout />
+                </DefaultLayout>
+              }>
+                <Route index element={<Navigate to="/" replace />} />
+                <Route path="login" element={<Login />} />
+                <Route path="signup" element={<Signup />} />
+              </Route>
+  
+              {/* Routes without header*/}
+              <Route path="/edit" element={<Edit />} />
+            </Routes>
+          </section>
+        </div>
+      </Router>
+    );
 }
 
 export default App;
